f5dee17c02250f0e94bd6bccb8d25936708664fd
Moritz Onken <onken@netcubed.de>
2011-05-21 23:17:56 +0200

    404 for favicon et al

--- lib/MetaCPAN/Web/Controller/Home.pm
+++ lib/MetaCPAN/Web/Controller/Home.pm
@@ -5,4 +5,14 @@ use base 'MetaCPAN::Web::Controller';
 
 sub endpoint { '/' }
 
+sub index {
+    my ( $self, $req ) = @_;
+    if( $req->path ne '/' ) {
+        my $cv = AE::cv;
+        $cv->send($self->not_found($req));
+        return $cv;
+    }
+    return $self->next::method($req);
+}
+
 1;
