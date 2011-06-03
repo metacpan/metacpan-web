f7ef73a81efa8cdb8b1300e018dbd97a5dabc945
Moritz Onken <onken@netcubed.de>
2011-06-03 15:53:34 +0200

    tabular presentation of releases

--- static/js/jquery.relatize_date.js
+++ static/js/jquery.relatize_date.js
@@ -67,18 +67,16 @@
      */
     distanceOfTimeInWords: function(fromTime, toTime, includeTime) {
       var delta = parseInt((toTime.getTime() - fromTime.getTime()) / 1000);
-      if (delta < 60) {
-          return 'less than a minute ago';
-      } else if (delta < 120) {
-          return 'about a minute ago';
+      if (delta < 120) {
+          return 'a minute ago';
       } else if (delta < (45*60)) {
           return (parseInt(delta / 60)).toString() + ' minutes ago';
       } else if (delta < (120*60)) {
-          return 'about an hour ago';
+          return 'an hour ago';
       } else if (delta < (24*60*60)) {
-          return 'about ' + (parseInt(delta / 3600)).toString() + ' hours ago';
+          return (parseInt(delta / 3600)).toString() + ' hours ago';
       } else if (delta < (48*60*60)) {
-          return '1 day ago';
+          return 'one day ago';
       } else {
         var days = (parseInt(delta / 86400)).toString();
         if (days > 5) {
