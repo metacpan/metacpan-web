151b70b4d1bfc0b66ec520fd7c85daae160e04e1
Moritz Onken <onken@netcubed.de>
2011-06-20 11:45:46 +0200

    Plack::Middleware::Assets is now on the CPAN

--- lib/MetaCPAN/Web.pm
+++ lib/MetaCPAN/Web.pm
@@ -26,6 +26,7 @@ my $app = Plack::App::URLMap->new;
 $app->map( '/static/' => Plack::App::File->new( root => 'static' ) );
 $app->map( '/' => $controller->dispatch );
 $app = Plack::Middleware::Runtime->wrap($app);
+$app = Plack::Middleware::Assets->wrap( $app, files => [<static/css/*.css>] );
 $app = Plack::Middleware::Assets->wrap(
     $app,
     files => [
@@ -33,7 +34,6 @@ $app = Plack::Middleware::Assets->wrap(
           qw(jquery.min jquery.cookie jquery.relatize_date jquery.ajaxQueue jquery.autocomplete.pack shCore shBrushPerl cpan)
     ]
 );
-$app = Plack::Middleware::Assets->wrap( $app, files => [<static/css/*.css>] );
 Plack::Middleware::StackTrace->wrap($app);
 
 Plack::Middleware::ReverseProxy->wrap($app);
