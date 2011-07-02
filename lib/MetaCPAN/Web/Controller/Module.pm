a7cb0a5afb00e152a7888055acd483c284db6c80
Moritz Onken <onken@netcubed.de>
2011-07-02 14:40:34 +0200

    fixed /module

--- lib/MetaCPAN/Web/Controller/Module.pm
+++ lib/MetaCPAN/Web/Controller/Module.pm
@@ -1,61 +1,38 @@
 package MetaCPAN::Web::Controller::Module;
-use strict;
-use warnings;
-use base 'MetaCPAN::Web::Controller';
+
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'MetaCPAN::Web::Controller' }
 use URI::Escape;
-use Scalar::Util qw(blessed);
 
-sub index {
-    my ( $self, $req ) = @_;
-    my $cv = AE::cv;
-    my ( undef, undef, @module ) = split( /\//, $req->path );
+sub index : PathPart('module') : Chained('/') : Args {
+    my ( $self, $c, @module ) = @_;
+    my $data
+        = @module == 1
+        ? $c->model('API::Module')->find(@module)->recv
+        : $c->model('API::Module')->get(@module)->recv;
 
-    my $data;
-    my $cond;
-    if ( @module == 1 ) {
-        $cond = $self->model('Module')->find( $module[0] );
-    }
-    elsif ( @module > 2 ) {
-        $cond = $self->model('Module')->get(@module);
-    }
-    else {
-        $cv->send( {} );
-        return;
-    }
+    $c->detach('/not_found') unless ( $data->{name} );
+    my $pod = $c->model('API')->request( '/pod/' . join( '/', @module ) );
+    my $release
+        = $c->model('API::Release')->get( $data->{author}, $data->{release} );
+    my $author = $c->model('API::Author')->get( $data->{author} );
+    my $versions
+        = $c->model('API::Release')->versions( $data->{distribution} );
+    ( $pod, $author, $release, $versions )
+        = ( $pod & $author & $release & $versions )->recv;
 
-    my $get = $cond->(
-        sub {
-            $data = shift->recv;
-            return $self->not_found($req) unless ( $data->{name} );
-            my $pod = $self->model->request( '/pod/' . join( '/', @module ) );
-            my $release =
-              $self->model('Release')->get( $data->{author}, $data->{release} );
-            my $author   = $self->model('Author')->get( $data->{author} );
-            my $versions = $self->model('Release')->versions( $data->{distribution} );
-            return ( $pod & $author & $release & $versions );
+    $c->stash(
+        {   module  => $data,
+            author  => $author,
+            pod     => $pod->{raw},
+            release => $release->{hits}->{hits}->[0]->{_source},
+            versions =>
+                [ map { $_->{fields} } @{ $versions->{hits}->{hits} } ],
+            template => 'module.html',
         }
     );
-
-    $get->(
-        sub {
-            my ( $pod, $author, $release, $versions ) = shift->recv;
-            if ( blessed $pod && $pod->isa('Plack::Response') ) {
-                $cv->send($pod);
-                return;
-            }
-            $cv->send(
-                {
-                    module  => $data,
-                    author  => $author,
-                    pod     => $pod->{raw},
-                    release => $release->{hits}->{hits}->[0]->{_source},
-                    versions =>
-                      [ map { $_->{fields} } @{ $versions->{hits}->{hits} } ],
-                }
-            );
-        }
-    );
-    return $cv;
 }
 
 1;
