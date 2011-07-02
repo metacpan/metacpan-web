895c6e56219c39bb2911053b4bf7c070874f4bea
Moritz Onken <onken@netcubed.de>
2011-07-02 16:29:25 +0200

    added session and user methods to request role

--- lib/MetaCPAN/Web/Role/Request.pm
+++ lib/MetaCPAN/Web/Role/Request.pm
@@ -2,6 +2,7 @@ package MetaCPAN::Web::Role::Request;
 
 use Moose::Role;
 use URI::Query;
+use Plack::Session;
 
 sub query_string_with {
     my $self   = shift;
@@ -17,8 +18,23 @@ sub page {
 }
 
 sub clone {
-  my ($self, %extra) = @_;
-  return (ref $self)->new({ %{$self->env}, %extra });
+    my ( $self, %extra ) = @_;
+    return ( ref $self )->new( { %{ $self->env }, %extra } );
+}
+
+sub session {
+    my $self = shift;
+    return Plack::Session->new( $self->env );
+}
+
+sub user {
+    my $self = shift;
+    $self->{user} = shift if (@_);
+    return $self->{user};
+}
+
+sub user_exists {
+    return shift->{user};
 }
 
 1;
