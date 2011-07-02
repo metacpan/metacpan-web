16944837825ae926c169ea17785b83d3aa6528b8
Moritz Onken <onken@netcubed.de>
2011-07-02 21:54:16 +0200

    fixed /login

--- lib/MetaCPAN/Web/Controller/Login.pm
+++ lib/MetaCPAN/Web/Controller/Login.pm
@@ -5,13 +5,11 @@ use namespace::autoclean;
 
 BEGIN { extends 'MetaCPAN::Web::Controller' }
 
-sub index {
+sub index :Path {
     my ( $self, $c ) = @_;
     my $code = $c->req->parameters->{code};
     return $c->detach('/not_found') unless ($code);
-
-    my $data = $self->model->request("/login/validate?code=$code")->recv;
-
+    my $data = $c->model('API')->request("/login/validate?code=$code")->recv;
     $c->req->session->set( msid => $data->{sid} );
     $c->res->redirect('/');
 }
