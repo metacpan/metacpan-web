f327e7afd1798175ffe4b2d17376eef36e3b058e
Moritz Onken <onken@netcubed.de>
2011-07-10 13:09:37 +0200

    delete identity controller action

--- lib/MetaCPAN/Web/Controller/Account.pm
+++ lib/MetaCPAN/Web/Controller/Account.pm
@@ -6,17 +6,23 @@ use namespace::autoclean;
 BEGIN { extends 'MetaCPAN::Web::Controller' }
 
 sub logout : Local {
-    my ($self, $c) = @_;
+    my ( $self, $c ) = @_;
     $c->req->session->expire;
     $c->res->redirect('/');
 }
 
 sub settings : Local {
-    my ($self, $c) = @_;
+    my ( $self, $c ) = @_;
 }
 
 sub identities : Local {
-    my ($self, $c) = @_;
+    my ( $self, $c ) = @_;
+    if ( my $delete = $c->req->params->{delete} ) {
+        $c->model('API::User')
+            ->delete_identity( $delete, $c->req->session->get('token') )
+            ->recv;
+        $c->res->redirect('/account/identities');
+    }
 }
 
 1;
