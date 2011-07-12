e91752698e9a20dc357cc4c477b93ff1ff89d1cc
Moritz Onken <onken@netcubed.de>
2011-07-12 14:37:16 +0200

    update and get profile api calls

--- lib/MetaCPAN/Web/Model/API/User.pm
+++ lib/MetaCPAN/Web/Model/API/User.pm
@@ -14,4 +14,14 @@ sub delete_identity {
     $self->request("/user/identity/$identity", undef, { method => 'DELETE', token => $token });
 }
 
+sub update_profile {
+    my ($self, $data, $token) = @_;
+    $self->request("/user/profile", $data, { method => 'PUT', token => $token });
+}
+
+sub get_profile {
+    my ($self, $token) = @_;
+    $self->request("/user/profile", undef, { token => $token });
+}
+
 1;
