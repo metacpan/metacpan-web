9c825d6bb83e5b189cef291f93e3c8651d847453
Moritz Onken <onken@netcubed.de>
2011-07-12 17:01:47 +0200

    profile form and controller

--- lib/MetaCPAN/Web/Controller/Account.pm
+++ lib/MetaCPAN/Web/Controller/Account.pm
@@ -2,6 +2,9 @@ package MetaCPAN::Web::Controller::Account;
 
 use Moose;
 use namespace::autoclean;
+use List::MoreUtils qw(pairwise);
+use DateTime ();
+use JSON::XS ();
 
 BEGIN { extends 'MetaCPAN::Web::Controller' }
 
@@ -18,10 +21,52 @@ sub settings : Local {
 sub identities : Local {
     my ( $self, $c ) = @_;
     if ( my $delete = $c->req->params->{delete} ) {
-        $c->model('API::User')
-            ->delete_identity( $delete, $c->req->session->get('token') )
-            ->recv;
+        $c->model('API::User')->delete_identity( $delete, $c->token )->recv;
         $c->res->redirect('/account/identities');
+    }
+}
+
+sub profile : Local {
+    my ( $self, $c ) = @_;
+    my $author = $c->model('API::User')->get_profile( $c->token )->recv;
+    $c->stash( { author => $author } );
+    my $req = $c->req;
+    return unless ( $req->method eq 'POST' );
+
+    my $data = $author;
+    $data->{blog} = [
+        pairwise { { url => $a, feed => $b } }
+        @{ [ $req->param('blog.url') ] },
+        @{ [ $req->param('blog.feed') ] }
+    ];
+    $data->{donation} = [
+        pairwise { { name => $a, id => $b } }
+        @{ [ $req->param('donation.name') ] },
+        @{ [ $req->param('donation.id') ] }
+    ];
+    $data->{profile} = [
+        pairwise { { name => $a, id => $b } }
+        @{ [ $req->param('profile.name') ] },
+        @{ [ $req->param('profile.id') ] }
+    ];
+    $data->{location}
+        = $req->params->{latitude}
+        ? [ $req->params->{latitude}, $req->params->{longitude} ]
+        : undef;
+    $data->{$_} = $req->params->{$_} eq "" ? undef : $req->params->{$_}
+        for (qw(name asciiname gravatar_url city region country));
+    $data->{$_} = [ grep {$_} $req->param($_) ] for (qw(website email));
+    $data->{extra}
+        = eval { JSON->new->relaxed->utf8->decode( $req->params->{extra} ) };
+    $data->{donation} = undef unless ( $req->params->{donations} );
+
+    my $res
+        = $c->model('API::User')->update_profile( $data, $c->token )->recv;
+    if ( $res->{error} ) {
+        $c->stash( { author => $data, errors => $res->{errors} } );
+    }
+    else {
+        $c->stash( { success => 1, author => $res } );
     }
 }
 
