3f2c59b616ac3a149de2daad180eea593aa3efe8
Moritz Onken <onken@netcubed.de>
2011-07-13 12:16:48 +0200

    typo

--- lib/MetaCPAN/Web/Controller/Login.pm
+++ lib/MetaCPAN/Web/Controller/Login.pm
@@ -7,18 +7,21 @@ BEGIN { extends 'MetaCPAN::Web::Controller' }
 
 sub index : Path {
     my ( $self, $c ) = @_;
-    my $code = $c->req->parameters->{code};
-    return $c->detach('/not_found') unless ($code);
-    my $data
-        = $c->model('API')
-        ->request( "/oauth2/access_token?client_id="
-            . $c->config->{consumer_id}
-            . "&client_secret="
-            . $c->config->{consumer_secret}
-            . "&code=$code" )->recv;
-    $c->req->session->set( token => $data->{access_token} );
-    $c->authenticate( { token => $data->{access_token} } );
-    $c->res->redirect('/');
+    if ( my $code = $c->req->parameters->{code} ) {
+        my $data
+            = $c->model('API')
+            ->request( "/oauth2/access_token?client_id="
+                . $c->config->{consumer_key}
+                . "&client_secret="
+                . $c->config->{consumer_secret}
+                . "&code=$code" )->recv;
+        $c->req->session->set( token => $data->{access_token} );
+        $c->authenticate( { token => $data->{access_token} } );
+        $c->res->redirect('/');
+    }
+    else {
+        $c->stash( { template => 'account/login.html' } );
+    }
 }
 
 1;
