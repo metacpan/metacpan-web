f9d76b55889b61a4d3e3387b2a96edc9ef578101
Lukas Mai <l.mai@web.de>
2011-08-09 14:27:13 +0200

    fix diff syntax highlighting
    An empty line could cause highlighting to bleed into the next line.

--- root/static/js/shBrushDiff.js
+++ root/static/js/shBrushDiff.js
@@ -22,12 +22,12 @@
 	function Brush()
 	{
 		this.regexList = [
-			{ regex: /^\+\+\+.*$/gm,		css: 'color2' },
-			{ regex: /^\-\-\-.*$/gm,		css: 'color2' },
-			{ regex: /^\s.*$/gm,			css: 'color1' },
-			{ regex: /^@@.*@@.*/gm,			css: 'variable' },
-			{ regex: /^\+[^\+]{1}.*$/gm,	css: 'string' },
-			{ regex: /^\-[^\-]{1}.*$/gm,	css: 'comments' }
+			{ regex: /^\+\+\+.*$/gm,        css: 'color2' },
+			{ regex: /^---.*$/gm,           css: 'color2' },
+			{ regex: /^(?=\s).*$/gm,        css: 'color1' },
+			{ regex: /^@@.*@@.*/gm,         css: 'variable' },
+			{ regex: /^\+(?!\+).*$/gm,      css: 'string' },
+			{ regex: /^-(?!-).*$/gm,        css: 'comments' }
 			];
 	};
 
