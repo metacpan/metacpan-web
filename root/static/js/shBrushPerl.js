9cb4055d7ef29476f508a9405e953bb8cc097a06
Lukas Mai <l.mai@web.de>
2011-08-16 18:14:04 +0200

    fix a few perl syntax highlighting bugs
    - comment highlighting could bleed into the next line, as seen on
      https://metacpan.org/source/ATRIPPS/File-Tail-Multi-0.1/Multi.pm
    
    - "#!" was treated as a shebang even in the middle of a line/file
    
    - "*" was missing from the list of variable sigils
    
    - __END__ was matched case-insensitively and __DATA__ was missing
    
    - general cleanup of the regexes

--- root/static/js/shBrushPerl.js
+++ root/static/js/shBrushPerl.js
@@ -7,7 +7,7 @@
  *
  * @version
  * 3.0.83 (July 02 2010)
- * 
+ *
  * @copyright
  * Copyright (C) 2004-2010 Alex Gorbatchev.
  *
@@ -22,54 +22,54 @@
 	function Brush()
 	{
 		// Contributed by David Simmons-Duffin and Marty Kube
-	
-		var funcs = 
-			'abs accept alarm atan2 bind binmode chdir chmod chomp chop chown chr ' + 
-			'chroot close closedir connect cos crypt defined delete each endgrent ' + 
-			'endhostent endnetent endprotoent endpwent endservent eof exec exists ' + 
-			'exp fcntl fileno flock fork format formline getc getgrent getgrgid ' + 
-			'getgrnam gethostbyaddr gethostbyname gethostent getlogin getnetbyaddr ' + 
-			'getnetbyname getnetent getpeername getpgrp getppid getpriority ' + 
-			'getprotobyname getprotobynumber getprotoent getpwent getpwnam getpwuid ' + 
-			'getservbyname getservbyport getservent getsockname getsockopt glob ' + 
-			'gmtime grep hex index int ioctl join keys kill lc lcfirst length link ' + 
-			'listen localtime lock log lstat map mkdir msgctl msgget msgrcv msgsnd ' + 
-			'oct open opendir ord pack pipe pop pos print printf prototype push ' + 
-			'quotemeta rand read readdir readline readlink readpipe recv rename ' + 
-			'reset reverse rewinddir rindex rmdir scalar seek seekdir select semctl ' + 
-			'semget semop send setgrent sethostent setnetent setpgrp setpriority ' + 
-			'setprotoent setpwent setservent setsockopt shift shmctl shmget shmread ' + 
-			'shmwrite shutdown sin sleep socket socketpair sort splice split sprintf ' + 
-			'sqrt srand stat study substr symlink syscall sysopen sysread sysseek ' + 
-			'system syswrite tell telldir time times tr truncate uc ucfirst umask ' + 
+
+		var funcs =
+			'abs accept alarm atan2 bind binmode chdir chmod chomp chop chown chr ' +
+			'chroot close closedir connect cos crypt defined delete each endgrent ' +
+			'endhostent endnetent endprotoent endpwent endservent eof exec exists ' +
+			'exp fcntl fileno flock fork format formline getc getgrent getgrgid ' +
+			'getgrnam gethostbyaddr gethostbyname gethostent getlogin getnetbyaddr ' +
+			'getnetbyname getnetent getpeername getpgrp getppid getpriority ' +
+			'getprotobyname getprotobynumber getprotoent getpwent getpwnam getpwuid ' +
+			'getservbyname getservbyport getservent getsockname getsockopt glob ' +
+			'gmtime grep hex index int ioctl join keys kill lc lcfirst length link ' +
+			'listen localtime lock log lstat map mkdir msgctl msgget msgrcv msgsnd ' +
+			'oct open opendir ord pack pipe pop pos print printf prototype push ' +
+			'quotemeta rand read readdir readline readlink readpipe recv rename ' +
+			'reset reverse rewinddir rindex rmdir scalar seek seekdir select semctl ' +
+			'semget semop send setgrent sethostent setnetent setpgrp setpriority ' +
+			'setprotoent setpwent setservent setsockopt shift shmctl shmget shmread ' +
+			'shmwrite shutdown sin sleep socket socketpair sort splice split sprintf ' +
+			'sqrt srand stat study substr symlink syscall sysopen sysread sysseek ' +
+			'system syswrite tell telldir time times tr truncate uc ucfirst umask ' +
 			'undef unlink unpack unshift utime values vec wait waitpid warn write';
-    
-		var keywords =  
+
+		var keywords =
 			'bless caller continue dbmclose dbmopen die do dump else elsif eval exit ' +
-			'for foreach goto if import last local my next no our package redo ref ' + 
+			'for foreach goto if import last local my next no our package redo ref ' +
 			'require return sub tie tied unless untie until use wantarray while ' +
 			// Try::Tiny
-			'try catch finally ' + 
+			'try catch finally ' +
 			// Moose
 			'has extends with before after around override augment';
-    
+
 		this.regexList = [
-			{ regex: new RegExp('#[^!].*$', 'gm'),					css: 'comments' },
-			{ regex: new RegExp('^\\s*#!.*$', 'gm'),				css: 'preprocessor' }, // shebang
-			{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,	css: 'string' },
-			{ regex: SyntaxHighlighter.regexLib.singleQuotedString,	css: 'string' },
-			{ regex: new RegExp('(\\$|@|%|\\$#)\\w+', 'g'),				css: 'variable' },
-			{ regex: new RegExp('__END__(.|\\n|\\r)*', 'gi'), css: 'comments' },
-			{ regex: new RegExp('^=\\w+(.|\\n|\\r)*?^=cut\\s*$', 'mg'), css: 'comments' },
-			{ regex: new RegExp(this.getKeywords(funcs), 'gm'),	css: 'functions' },
-			{ regex: new RegExp(this.getKeywords(keywords), 'gm'),	css: 'keyword' }
+			{ regex: /#.*$/gm, css: 'comments' },
+			{ regex: /^#!.*\n/g, css: 'preprocessor' }, // shebang
+			{ regex: SyntaxHighlighter.regexLib.doubleQuotedString, css: 'string' },
+			{ regex: SyntaxHighlighter.regexLib.singleQuotedString, css: 'string' },
+			{ regex: /(?:[$@%*]|\$#)\w+/g, css: 'variable' },
+			{ regex: /\b__(?:END|DATA)__\b[\s\S]*$/g, css: 'comments' },
+			{ regex: /^=\w[\s\S]*?\n=cut\s*$/gm, css: 'comments' },
+			{ regex: new RegExp(this.getKeywords(funcs), 'gm'), css: 'functions' },
+			{ regex: new RegExp(this.getKeywords(keywords), 'gm'), css: 'keyword' }
 		    ];
 
 		this.forHtmlScript(SyntaxHighlighter.regexLib.phpScriptTags);
 	}
 
-	Brush.prototype	= new SyntaxHighlighter.Highlighter();
-	Brush.aliases		= ['perl', 'Perl', 'pl'];
+	Brush.prototype = new SyntaxHighlighter.Highlighter();
+	Brush.aliases = ['perl', 'Perl', 'pl'];
 
 	SyntaxHighlighter.brushes.Perl = Brush;
 
