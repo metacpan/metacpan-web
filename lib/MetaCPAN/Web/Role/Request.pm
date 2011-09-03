2eab9680feb8fce982edd15aec45806186db1f65
Florian Ragwitz <rafl@debian.org>
2011-09-03 14:17:08 +0200

    Nuke an unused request method

--- lib/MetaCPAN/Web/Role/Request.pm
+++ lib/MetaCPAN/Web/Role/Request.pm
@@ -8,11 +8,6 @@ sub page {
     return $page && $page =~ /^\d+$/ ? $page : 1;
 }
 
-sub clone {
-    my ( $self, %extra ) = @_;
-    return ( ref $self )->new( { %{ $self->env }, %extra } );
-}
-
 sub session {
     my $self = shift;
     return Plack::Session->new( $self->env );
