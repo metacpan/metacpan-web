bdfb3f50371b961f017c8b78776187b7121e2eab
Olaf Alders <olaf@wundersolutions.com>
2011-10-18 13:43:42 -0400

    Adds HTML whitelisting to Pod under /module

--- lib/MetaCPAN/Web/Controller/Module.pm
+++ lib/MetaCPAN/Web/Controller/Module.pm
@@ -2,6 +2,7 @@ package MetaCPAN::Web::Controller::Module;
 
 use Moose;
 use namespace::autoclean;
+use HTML::Restrict;
 
 BEGIN { extends 'MetaCPAN::Web::Controller' }
 
@@ -28,9 +29,47 @@ sub index : PathPart('module') : Chained('/') : Args {
     $self->stash_api_results($c, $reqs, $data);
     $self->add_favorites_data($data, $reqs->{favorites}, $data);
 
+    my $hr = HTML::Restrict->new;
+    $hr->set_rules(
+        {   a       => [qw( href target )],
+            b       => [],
+            br      => [],
+            caption => [],
+            center  => [],
+            code    => [],
+            dd      => ['id'],
+            div     => [qw(style)],
+            dl      => ['id'],
+            dt      => ['id'],
+            em      => [],
+            h1      => ['id'],
+            h2      => ['id'],
+            h3      => ['id'],
+            h4      => ['id'],
+            h5      => ['id'],
+            h6      => ['id'],
+            i       => [],
+            img     => [qw( alt border height width src style / )],
+            li      => ['id'],
+            ol      => [],
+            p       => [qw(style)],
+            pre     => [qw(id class)],
+            span    => [qw(style)],
+            strong  => [],
+            sub     => [],
+            sup     => [],
+            table   => [qw( style border cellspacing cellpadding align )],
+            tbody   => [],
+            td      => [],
+            tr      => [],
+            u       => [],
+            ul      => ['id'],
+        }
+    );
+
     $c->stash(
         {   module  => $data,
-            pod     => $reqs->{pod}->{raw},
+            pod     => $hr->process( $reqs->{pod}->{raw} ),
             release => $reqs->{release}->{hits}->{hits}->[0]->{_source},
             template => 'module.html',
         }
