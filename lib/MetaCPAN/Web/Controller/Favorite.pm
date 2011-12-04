e1dcca75587e9805a2063fea54d98fb48e83c45b
Florian Ragwitz <rafl@debian.org>
2011-12-04 04:01:20 +0100

    Y U NO RESPONDING?
    Because waiting for something that's never going to happen usually takes a long
    time. Let's try not doing that.
    
    With this, the problem of our starman workers locking up until no active workers
    are available anymore and the whole frontend grinds to a halt should disappear.

--- lib/MetaCPAN/Web/Controller/Favorite.pm
+++ lib/MetaCPAN/Web/Controller/Favorite.pm
@@ -6,36 +6,28 @@ use base 'MetaCPAN::Web::Controller';
 
 sub recent : Path('/favorite/recent') {
     my ( $self, $c ) = @_;
-    my $cv = AE::cv();
-    $c->model('API::Favorite')->recent( $c->req->page )->(
-        sub {
-            my ($data) = shift->recv;
-            my $latest = [ map { $_->{_source} } @{ $data->{hits}->{hits} } ];
-            $cv->send(
-                {   recent => $latest, took => $data->{took},
-                    total  => $data->{hits}->{total}
-                }
-            );
-        }
-    );
-    $c->stash({%{$cv->recv}, template => 'favorite/recent.html'});
+
+    my $data = $c->model('API::Favorite')->recent( $c->req->page )->recv;
+
+    $c->stash({
+        recent   => [ map { $_->{_source} } @{ $data->{hits}->{hits} } ],
+        took     => $data->{took},
+        total    => $data->{hits}->{total},
+        template => 'favorite/recent.html',
+    });
 }
 
 sub index : Path('/favorite/leaderboard') {
     my ( $self, $c ) = @_;
-    my $cv = AE::cv();
-    $c->model( 'API::Favorite' )->leaderboard( $c->req->page )->(
-        sub {
-            my ( $data ) = shift->recv;
-            $cv->send(
-                {   leaders => $data->{facets}->{leaderboard}->{terms},
-                    took    => $data->{took},
-                    total   => $data->{hits}->{total}
-                }
-            );
-        }
-    );
-    $c->stash( { %{ $cv->recv }, template => 'favorite/leaderboard.html' } );
+
+    my $data = $c->model( 'API::Favorite' )->leaderboard( $c->req->page )->recv;
+
+    $c->stash({
+        leaders  => $data->{facets}->{leaderboard}->{terms},
+        took     => $data->{took},
+        total    => $data->{hits}->{totaL},
+        template => 'favorite/leaderboard.html',
+    });
 }
 
 1;
