f47b133514ba3f3a715af66a3d4adddd2bcbdac6
Moritz Onken <onken@netcubed.de>
2013-01-27 07:13:07 -0500

    fix sticky navbar

--- root/static/js/toolbar.js
+++ root/static/js/toolbar.js
@@ -1,15 +1,15 @@
 if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
     $(function() {
-        var el = $('.search-bar');
+        var el = $('.nav-list').first();
         if (!el.length) return;
         var originalTop = el.offset().top; // store original top position
         var height = el.height();
         $(window).scroll(function(e) {
             var screenHeight = $(window).height();
-            if ($(this).scrollTop() > originalTop + (screenHeight - height < 0 ? height - screenHeight + 10 : -10 )) {
+            if ($(this).scrollTop() > originalTop + (screenHeight - height < 0 ? height - screenHeight : 0 )) {
                 el.css({
                     'position': 'fixed',
-                    'top': (screenHeight - height < 0 ? screenHeight - height - 10 : 10 ) + 'px'
+                    'top': (screenHeight - height < 0 ? screenHeight - height : 0 ) + 'px'
                 });
             } else {
                 el.css({
