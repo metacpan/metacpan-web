2bd76c59de9da89c0c42b9505949316a2980bee5
Yanick Champoux <yanick@babyl.dyndns.org>
2013-03-12 22:02:22 -0400

    First stab at reports for CPAN-API/cpan-api#253
    I'll let people fight over nomenclature. Meanwhile here's some first
    stab at the report. :-)
    
    I'm still not sure that per-module makes more
    sense than per-distribution, but just for the sake of having something
    running, I've assuming that the data is per module. If that change,
    the logic will be easy enough to move around.
    
    I'm not terribly happy about the name of the keys, they'll probably
    have to be changed.
    
    I've also sorted the recommendations per total activity, and report both
    the recommendations in favor of alternatives and against in the same
    list, which seems to me the most succinct way to present the information.

--- lib/MetaCPAN/Web/Controller/Module.pm
+++ lib/MetaCPAN/Web/Controller/Module.pm
@@ -29,4 +29,66 @@ sub redirect_to_pod : PathPart('module') : Chained('/') : Args {
     $c->detach();
 }
 
+=pod
+
+For giggles and to get the ball running, I'm assuming that recommendations are per-modules.
+
+I'm also assuming that we have the modules recommended over this one, and
+those under it. And that the whole this has the format:
+
+    $data->{recommendations} = {
+        recommended_over => {
+            'Foo'       => [ 'AUTHOR1', 'AUTHOR2' ],
+            'Bar::Baz'  => [ 'AUTHOR3' ],
+        },
+        rather_use => {
+            'Frob::Uscate' => [ 'AUTHOR4' ],
+        },
+    };
+
+Yes, 'rather_use' is a crappy key, but for now it'll do. We'll s/// it to
+something decent down the line.
+
+=cut
+
+sub groom_recommendations {
+    my( $self, $c, $data ) = @_;
+
+    # sample data
+    $data->{recommendations} = {
+        recommended_over => {
+            'Foo'       => [ 'AUTHOR1', 'AUTHOR2' ],
+            'Bar::Baz'  => [ 'AUTHOR3' ],
+            'Frob::Uscate' => [ 'AUTHOR5', 'AUTHOR6' ],
+        },
+        rather_use => {
+            'Frob::Uscate' => [ 'AUTHOR4' ],
+        },
+    };
+
+    my $r = $data->{recommendations} or return [];
+
+    my %rec;
+
+    if ( my $plus = $r->{recommended_over} ) {
+        while( my ($module,$votes) = each %$plus ) {
+            $rec{$module}{module} = $module;
+            $rec{$module}{sum} = $rec{$module}{plus} = scalar @$votes;
+        }
+    }
+
+    if ( my $minus = $r->{rather_use} ) {
+        while( my ($module,$votes) = each %$minus ) {
+            $rec{$module}{module} = $module;
+            $rec{$module}{minus} = scalar @$votes;
+            $rec{$module}{sum}  += scalar @$votes;
+        }
+    }
+
+    return [
+        reverse sort { $a->{sum} <=> $b->{sum} } values %rec
+    ];
+
+}
+
 1;
