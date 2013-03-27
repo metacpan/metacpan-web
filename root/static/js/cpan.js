7797d3606c5ca5322e42a924a72c9fdec2e20486
Moritz Onken <onken@netcubed.de>
2013-03-26 23:03:11 -0400

    polish account page

--- root/static/js/cpan.js
+++ root/static/js/cpan.js
@@ -199,7 +199,7 @@ $(document).ready(function() {
         }
       }
 
-    $('.pod h1,h2,h3,h4,h5,h6').each(function() {
+    $('.pod').find('h1,h2,h3,h4,h5,h6').each(function() {
       $(this).wrapInner(function() {
         return '<a href="#___pod"></a>';
       });
