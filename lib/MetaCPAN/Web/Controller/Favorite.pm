706f0ed53599b195af2f38ea985c31f07f07edd3
Olaf Alders <olaf@wundersolutions.com>
2013-03-28 00:40:24 -0400

    Identify authors who have clicked ++ in the recent ++ feed.

--- lib/MetaCPAN/Web/Controller/Favorite.pm
+++ lib/MetaCPAN/Web/Controller/Favorite.pm
@@ -3,14 +3,29 @@ use strict;
 use warnings;
 use base 'MetaCPAN::Web::Controller';
 
-
 sub recent : Path('/favorite/recent') {
     my ( $self, $c ) = @_;
 
-    my $data = $c->model('API::Favorite')->recent( $c->req->page )->recv;
+    my $data = $c->model( 'API::Favorite' )->recent( $c->req->page )->recv;
+    my @faves = map { $_->{_source} } @{ $data->{hits}->{hits} };
+    my @user_ids = map { $_->{user} } @faves;
+
+    my $authors
+        = $c->model( 'API::Author' )->by_user( \@user_ids )->recv->{hits}
+        ->{hits};
+
+    my %author_for_user_id
+        = map { $_->{fields}->{user} => $_->{fields}->{pauseid} } @{$authors};
+
+    foreach my $fave ( @faves ) {
+        if ( exists $author_for_user_id{ $fave->{user} } ) {
+            $fave->{clicked_by_author} = $author_for_user_id{ $fave->{user} };
+        }
+    }
 
     $c->stash({
-        recent   => [ map { $_->{_source} } @{ $data->{hits}->{hits} } ],
+        header   => 1,
+        recent   => \@faves,
         took     => $data->{took},
         total    => $data->{hits}->{total},
         template => 'favorite/recent.html',
