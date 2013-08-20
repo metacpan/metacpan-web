97bde92543cddd3776c187dd6ced71c42a8a68db
Alex Beamish <talexb@gmail.com>
2013-08-20 15:14:50 -0400

    Limit release to latest ones only
    Previous version dumped all releases; releases are now limited to just the
    latest ones, bringing that file in line with the size of the others.

--- bin/generate_sitemap.pl
+++ bin/generate_sitemap.pl
@@ -12,19 +12,33 @@ use MetaCPAN::Sitemap;
 
 {
     my @parts = (
+
+	#  For authors, we're looking for the pauseid, and want to build a URL
+	#  with 'author' in the path.
+
         {   objectType    => 'author',
             fieldName     => 'pauseid',
             xmlFile       => '/tmp/authors.xml',
             cpanDirectory => 'author',
         },
+
+	#  For distributions, we're looking for the distribution name, and we
+	#  want to build a URL with 'module' in the path.
+
         {   objectType    => 'distribution',
             fieldName     => 'name',
             xmlFile       => '/tmp/modules.xml',
             cpanDirectory => 'module',
         },
+
+	#  For releases, we're looking for a download URL; since we're not
+	#  building a URL, the cpanDirectory is missing, but we also want to
+	#  filter on only the 'latest' entries.
+
         {   objectType => 'release',
             fieldName  => 'download_url',
             xmlFile    => '/tmp/releases.xml',
+	    filter     => { status => 'latest' },
         }
     );
 
