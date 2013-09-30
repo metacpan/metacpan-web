e8f99d389745502fc07d7305da3a4e5b53897b7c
Randy Stauner <randy@magnificent-tears.com>
2013-09-29 19:54:52 -0700

    Redirect old /module urls to new /pod urls

---
+++ lib/MetaCPAN/Web/Controller/Module.pm
@@ -0,0 +1,30 @@
+package MetaCPAN::Web::Controller::Module;
+
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'MetaCPAN::Web::Controller' }
+
+# NOTE: We may (be able to) put these redirects into nginx
+# but it's nice to have them here (additionally) for development.
+
+sub redirect_to_pod : PathPart('module') : Chained('/') : Args {
+    my ( $self, $c, @path ) = @_;
+
+    # Forward old '/module/' links to the new '/pod/' controller.
+
+    # /module/AUTHOR/Release-0.0/lib/Foo/Bar.pm
+    if ( @path > 1 ) {
+        # FIXME: /pod/release
+        # Force the author arg to uppercase to avoid another redirect.
+        $c->res->redirect( '/pod/' . join( '/', uc(shift @path), @path ), 301 );
+    }
+    # /module/Foo::Bar
+    else {
+        $c->res->redirect( '/pod/' . join( '/', @path ), 301 );
+    }
+
+    $c->detach();
+}
+
+1;
