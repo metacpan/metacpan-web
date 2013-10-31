a8e1e1d1b36d4e85f15bde484df939e560322a70
Michiel Beijen <michiel.beijen@otrs.com>
2013-10-31 13:30:55 +0100

    Changed links to value rather than key.
    All other links are on the value; this puts all the links on the
    right-hand side.

--- root/static/js/github.js
+++ root/static/js/github.js
@@ -48,8 +48,8 @@ function Github() {
 
                             // with v3 api the number under 'watchers' is actually the number of stargazers
                             // in the v4 api this will be corrected. see https://github.com/CPAN-API/metacpan-web/issues/975
-                            +'  <tr><th><a href="'+ data.html_url +'/stargazers">Stars</a>:</th><td>'+ data.watchers +'</td></tr>'
-                            +'  <tr><th><a href="'+ data.html_url +'/network">Forks</a>:</th><td>'+ data.forks +'</td></tr>'
+                            +'  <tr><th>Stars:</th><td><a href="'+ data.html_url +'/stargazers">'+ data.watchers +'</a></td></tr>'
+                            +'  <tr><th>Forks:</th><td><a href="'+ data.html_url +'/network">'+ data.forks +'</a></td></tr>'
 
                             +( data.has_issues
                             ?'  <tr><th>Open <a href="'+ data.html_url +'/issues">Issues</a>:</th><td>'+ data.open_issues +'</td></tr>'
