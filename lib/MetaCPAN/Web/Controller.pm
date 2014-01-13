0cdef661286fad7cf40f2923524a505111793ea0
Randy Stauner <randy@magnificent-tears.com>
2014-01-13 09:05:43 -0700

    Remove unnecessary Catalyst::Controller::ActionRole
    It's functionality was merged into Catalyst 5.90013.

--- lib/MetaCPAN/Web/Controller.pm
+++ lib/MetaCPAN/Web/Controller.pm
@@ -2,6 +2,6 @@ package MetaCPAN::Web::Controller;
 use Moose;
 use namespace::autoclean;
 
-BEGIN {extends 'Catalyst::Controller::ActionRole'; }
+BEGIN {extends 'Catalyst::Controller'; }
 
 __PACKAGE__->meta->make_immutable;
