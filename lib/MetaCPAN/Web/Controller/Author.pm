7b75e906ca6e9a9f29eab9a308a60cdaa33e797e
Olaf Alders <olaf@wundersolutions.com>
2014-01-30 20:08:10 -0500

    Tidy Author controller.

--- lib/MetaCPAN/Web/Controller/Author.pm
+++ lib/MetaCPAN/Web/Controller/Author.pm
@@ -11,29 +11,31 @@ sub index : Path : Args(1) {
     my ( $self, $c, $id ) = @_;
 
     # force consistent casing in URLs
-    if ( $id ne uc( $id ) ) {
-        $c->res->redirect( '/author/' . uc( $id ), 301 );
+    if ( $id ne uc($id) ) {
+        $c->res->redirect( '/author/' . uc($id), 301 );
         $c->detach;
     }
 
-    my $author_cv = $c->model( 'API::Author' )->get( $id );
+    my $author_cv = $c->model('API::Author')->get($id);
 
-    my $releases_cv = $c->model( 'API::Release' )->latest_by_author($id);
+    my $releases_cv = $c->model('API::Release')->latest_by_author($id);
 
     my ( $author, $data ) = ( $author_cv->recv, $releases_cv->recv );
-    $c->detach( '/not_found' ) unless ( $author->{pauseid} );
+    $c->detach('/not_found') unless ( $author->{pauseid} );
 
-    my $faves_cv = $c->model( 'API::Favorite' )->by_user($author->{user});
+    my $faves_cv = $c->model('API::Favorite')->by_user( $author->{user} );
 
     my $faves_data = $faves_cv->recv;
-    my $faves = [ sort { $b->{date} cmp $a->{date} }
-            map { $_->{fields} } @{ $faves_data->{hits}{hits} } ];
+    my $faves      = [
+        sort { $b->{date} cmp $a->{date} }
+        map  { $_->{fields} } @{ $faves_data->{hits}{hits} }
+    ];
 
     my $releases = [ map { $_->{fields} } @{ $data->{hits}->{hits} } ];
     my $date = List::Util::max
         map { DateTime::Format::ISO8601->parse_datetime( $_->{date} ) }
         @$releases;
-    $c->res->last_modified( $date );
+    $c->res->last_modified($date);
 
     $c->stash(
         {   author      => $author,
