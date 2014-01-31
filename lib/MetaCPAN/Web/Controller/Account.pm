2b2f0ff02ee19c34bf7cdacf900cd8530c524564
Olaf Alders <olaf@wundersolutions.com>
2014-01-30 20:19:24 -0500

    Tidies Account controller.

--- lib/MetaCPAN/Web/Controller/Account.pm
+++ lib/MetaCPAN/Web/Controller/Account.pm
@@ -45,21 +45,24 @@ sub profile : Local {
     return unless ( $req->method eq 'POST' );
 
     my $data = $author;
-    $data->{blog} = $req->param('blog.url')
+    $data->{blog}
+        = $req->param('blog.url')
         ? [
         pairwise { { url => $a, feed => $b } }
         @{ [ $req->param('blog.url') ] },
         @{ [ $req->param('blog.feed') ] }
         ]
         : undef;
-    $data->{donation} = $req->param('donation.name')
+    $data->{donation}
+        = $req->param('donation.name')
         ? [
         pairwise { { name => $a, id => $b } }
         @{ [ $req->param('donation.name') ] },
         @{ [ $req->param('donation.id') ] }
         ]
         : undef;
-    $data->{profile} = $req->param('profile.name')
+    $data->{profile}
+        = $req->param('profile.name')
         ? [
         pairwise { { name => $a, id => $b } }
         @{ [ $req->param('profile.name') ] },
