ecfaf1939573cac440dd48bd198a1e582e18354b
Olaf Alders <olaf@wundersolutions.com>
2014-03-13 18:02:42 +0100

    Adds /author/*/releases endpoint.

--- lib/MetaCPAN/Web/Controller/Author.pm
+++ lib/MetaCPAN/Web/Controller/Author.pm
@@ -44,7 +44,7 @@ sub index : Path : Args(1) {
             releases    => $releases,
             faves       => $faves,
             show_author => 1,
-            took        => $data->{took} + ($faves_data->{took} || 0),
+            took        => $data->{took} + ( $faves_data->{took} || 0 ),
             total       => $data->{hits}->{total},
             template    => 'author.html'
         }
@@ -53,6 +53,19 @@ sub index : Path : Args(1) {
     $c->stash( author_country_name =>
             Locale::Country::code2country( $author->{country} ) )
         if $author->{country};
+}
+
+sub releases : Path : Args(2) {
+    my ( $self, $c, $id, $page ) = @_;
+
+    my $author_cv   = $c->model('API::Author')->get($id);
+    my $releases_cv = $c->model('API::Release')->all_by_author($id);
+
+    my ( $author, $releases ) = ( $author_cv->recv, $releases_cv->recv );
+
+    my @releases = map { $_->{fields} } @{ $releases->{hits}->{hits} };
+
+    $c->stash( { releases => \@releases, author => $author, } );
 }
 
 __PACKAGE__->meta->make_immutable;
