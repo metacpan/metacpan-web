4c152a5967623cef66b99a8ef56968d213a005bd
Talina Shrotriya <talina.shrotriya@gmail.com>
2014-03-14 00:29:48 +0530

    Create generate_sitemap.pl

---
+++ bin/generate_sitemap.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/env perl
+
+#  Generate the sitemap XML files for the robots.txt file.
+
+use strict;
+use warnings;
+
+use FindBin qw ($Bin);
+use lib "$Bin/../lib";
+
+use MetaCPAN::Sitemap;
+
+
+    my @parts = (
+
+	#  For authors, we're looking for the pauseid, and want to build a URL
+	#  with 'author' in the path.
+
+        {   objectType    => 'author',
+            fieldName     => 'pauseid',
+            xmlFile       => '/tmp/authors.xml.gz',
+            cpanDirectory => 'author',
+        },
+
+	#  For distributions, we're looking for the distribution name, and we
+	#  want to build a URL with 'module' in the path.
+
+        {   objectType    => 'pod',
+            fieldName     => 'name',
+            xmlFile       => '/tmp/modules.xml.gz',
+            cpanDirectory => 'pod',
+        },
+
+	#  For releases, we're looking for a download URL; since we're not
+	#  building a URL, the cpanDirectory is missing, but we also want to
+	#  filter on only the 'latest' entries.
+
+        {   objectType => 'release',
+            fieldName  => 'distribution',
+            xmlFile    => '/tmp/releases.xml.gz',
+	    filter     => { status => 'latest' },
+	    cpanDirectory => 'release',
+        }
+    );
+
+    foreach my $part ( @parts ) {
+        MetaCPAN::Sitemap::process ( $part );
+    }
+	
