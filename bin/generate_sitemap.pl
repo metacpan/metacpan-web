ad78eeea3d2c94057a7f74cd6ce47eebda7bf0d0
Talina Shrotriya <talina.shrotriya@gmail.com>
2014-03-14 12:25:57 +0530

    Update generate_sitemap.pl

--- bin/generate_sitemap.pl
+++ bin/generate_sitemap.pl
@@ -4,46 +4,42 @@
 
 use strict;
 use warnings;
-
+use Data::Dumper;
 use FindBin qw ($Bin);
 use lib "$Bin/../lib";
 
 use MetaCPAN::Sitemap;
 
+my @parts = (
 
-    my @parts = (
+    #  For authors, we're looking for the pauseid, and want to build a URL
+    #  with 'author' in the path.
 
-	#  For authors, we're looking for the pauseid, and want to build a URL
-	#  with 'author' in the path.
+    {   object_type    => 'author',
+        field_name     => 'pauseid',
+        xml_file       => '/tmp/authors.xml.gz',
+        cpan_directory => 'author',
+    },
 
-        {   objectType    => 'author',
-            fieldName     => 'pauseid',
-            xmlFile       => '/tmp/authors.xml.gz',
-            cpanDirectory => 'author',
-        },
+    #  For distributions, we're looking for the distribution name, and we
+    #  want to build a URL with 'module' in the path.
 
-	#  For distributions, we're looking for the distribution name, and we
-	#  want to build a URL with 'module' in the path.
+    {   object_type    => 'distribution',
+        field_name     => 'name',
+        xml_file       => '/tmp/modules.xml.gz',
+        cpan_directory => 'pod',
+    },
 
-        {   objectType    => 'pod',
-            fieldName     => 'name',
-            xmlFile       => '/tmp/modules.xml.gz',
-            cpanDirectory => 'pod',
-        },
+    #  For releases, we're looking for a download URL; since we're not
+    #  building a URL, the cpan_directory is missing, but we also want to
+    #  filter on only the 'latest' entries.
 
-	#  For releases, we're looking for a download URL; since we're not
-	#  building a URL, the cpanDirectory is missing, but we also want to
-	#  filter on only the 'latest' entries.
-
-        {   objectType => 'release',
-            fieldName  => 'distribution',
-            xmlFile    => '/tmp/releases.xml.gz',
-	    filter     => { status => 'latest' },
-	    cpanDirectory => 'release',
-        }
-    );
-
-    foreach my $part ( @parts ) {
-        MetaCPAN::Sitemap::process ( $part );
+    {   object_type    => 'release',
+        field_name     => 'distribution',
+        xml_file       => '/tmp/releases.xml.gz',
+        cpan_directory => 'release',
+        filter         => { status => 'latest' },
     }
-	
+);
+
+MetaCPAN::Sitemap->new($_)->process for @parts;
