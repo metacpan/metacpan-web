b2f9d944a10f0235752398240ba1fd7a4750b7fc
Olaf Alders <olaf@wundersolutions.com>
2014-03-15 12:20:56 +0100

    Set up MooseX types which we'd like to import.

---
+++ lib/MetaCPAN/Web/Types.pm
@@ -0,0 +1,17 @@
+package MetaCPAN::Web::Types;
+
+use strict;
+use warnings;
+
+use parent 'MooseX::Types::Combine';
+
+__PACKAGE__->provide_types_from(
+    qw(
+        MooseX::Types::Common::Numeric
+        MooseX::Types::Common::String
+        MooseX::Types::Moose
+        MooseX::Types::URI
+        )
+);
+
+1;
