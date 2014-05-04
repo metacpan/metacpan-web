044c33ac57c2f8172fe96e7b115aedc61d1cac81
Talina06 <talina@talina.(none)>
2014-05-05 00:31:02 +0530

    Search for plusser added.

--- lib/MetaCPAN/Web/Model/API/Favorite.pm
+++ lib/MetaCPAN/Web/Model/API/Favorite.pm
@@ -81,6 +81,20 @@ sub by_user {
     );
 }
 
+#to search for v0/favorite/_search/{user} for the particular $distribution.
+sub by_dist {
+   my ($self, $distribution) = @_;
+   return $self->request(
+        '/favorite/_search',
+        {
+            query  => { match_all => {} },
+            filter => { term      => { distribution => $distribution }, },
+            fields => [qw(user)],
+            size   => 1000,
+        }
+    );
+}
+
 sub recent {
     my ( $self, $page ) = @_;
     $self->request(
