fe5c45ef8abf9b46375386c6b74481fd6e878586
Gabor Szabo <gabor@szabgab.com>
2014-05-19 00:45:24 +0300

    use JSON::XS instead of JSON

--- lib/MetaCPAN/Web/Model/API.pm
+++ lib/MetaCPAN/Web/Model/API.pm
@@ -6,7 +6,7 @@ extends 'Catalyst::Model';
 has [qw(api api_secure)] => ( is => 'ro' );
 
 use Encode ();
-use JSON;
+use JSON::XS qw(encode_json decode_json);
 use HTTP::Request ();
 use AnyEvent::Curl::Multi;
 use Try::Tiny 0.09;
