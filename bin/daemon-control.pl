5bfa4b02ae4f6e4811d6d4a8f692fbae30a6a2bb
Randy Stauner <randy@magnificent-tears.com>
2014-06-05 07:24:03 -0700

    Make dirs that daemon expects

--- bin/daemon-control.pl
+++ bin/daemon-control.pl
@@ -6,12 +6,17 @@ use strict;
 use warnings;
 
 use Daemon::Control;
+use File::Path 2.06 (); # core
 use Sys::Hostname qw( hostname );
 
 my $name    = 'metacpan-www';
 my $user    = 'metacpan';
 my $root    = '/home/metacpan';
 my $home    = "$root/metacpan.org";
+my %dirs    = (
+  pid => "$home/var/run",
+  log => "$home/var/log",
+);
 my $carton  = '/usr/local/perlbrew/perls/perl-5.16.2/bin/carton';
 my $workers = 7;
 
@@ -30,6 +35,8 @@ my @program_args = (
     '-s', => 'Starman',
 );
 
+File::Path::make_path(values %dirs);
+
 # Notes on unused args
 # scan_name: seems to be just 'starman master' (not useful)
 # stdout_file: always seems to be just empty
@@ -43,10 +50,10 @@ my $args = {
     lsb_sdesc    => "Starts $name",
     name         => $name,
     path         => "$home/bin/daemon-control.pl",
-    pid_file     => "$home/var/run/$name.pid",
+    pid_file     => "$dirs{pid}/$name.pid",
     program      => $carton,
     program_args => \@program_args,
-    stderr_file  => "$home/var/logs/starman_error.log",
+    stderr_file  => "$dirs{log}/starman_error.log",
     user         => $user,
 };
 
