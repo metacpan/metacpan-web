92ff0e27a9eb374a384056794a90999fcc889f2d
Graham Knop <haarg@haarg.org>
2014-09-10 15:52:22 -0400

    use fontawesome instead of glyphicons in more places

--- root/static/js/cpan.js
+++ root/static/js/cpan.js
@@ -245,7 +245,7 @@ $(document).ready(function () {
 
     $('.anchors').find('h1,h2,h3,h4,h5,h6,dt').each(function () {
       if (this.id) {
-        $(this).prepend('<a href="#'+this.id+'" class="anchor"><span class="glyphicon glyphicon-bookmark black"></span></a>');
+        $(this).prepend('<a href="#'+this.id+'" class="anchor"><span class="fa fa-bookmark black"></span></a>');
       }
     });
 
