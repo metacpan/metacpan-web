3e852e3fc9dd41098300c55465e28ec20d60922d
Graham Knop <haarg@haarg.org>
2014-09-11 16:28:32 -0400

    use classes for left panel sticky scrolling

--- root/static/js/toolbar.js
+++ root/static/js/toolbar.js
@@ -1,24 +1,27 @@
 if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
     $(function() {
-        var el = $('.nav-list').first();
+        var el = $('.nav-list.slidepanel').first();
         var topRow = $('.main-content').first();
         if (!el.length) return;
-        var originalTop = el.offset().top - topRow.offset().top; // store original top position - top content
         var height = el.height();
-        var contentHeight = $("div.content").height();
-        if(height > contentHeight) return;
+        var content = $("div.content");
+        if(height > content.height()) return;
+
         $(window).scroll(function(e) {
-            var screenHeight = $(window).height();
-            if ($(this).scrollTop() > topRow.offset().top + originalTop + (screenHeight - height < 0 ? height - screenHeight : 0 )) {
-                el.css({
-                    position: 'fixed',
-                    top: (screenHeight - height < 0 ? screenHeight - height : 0 ) + 'px'
-                });
-            } else {
-                el.css({
-                    position: 'absolute',
-                    top: originalTop
-                });
+            var scrollTop = $(this).scrollTop();
+            var contentTop = content.offset().top;
+            var contentHeight = content.height();
+            if (height > contentHeight) {
+                el.addClass("sticky-panel-top").removeClass("sticky-panel-bottom sticky-panel-sticky");
+            }
+            else if (scrollTop + height > contentTop + contentHeight) {
+                el.addClass("sticky-panel-bottom").removeClass("sticky-panel-top sticky-panel-sticky");
+            }
+            else if (scrollTop > contentTop) {
+                el.addClass("sticky-panel-sticky").removeClass("sticky-panel-top sticky-panel-bottom");
+            }
+            else {
+                el.addClass("sticky-panel-top").removeClass("sticky-panel-bottom sticky-panel-sticky");
             }
         });
     });
