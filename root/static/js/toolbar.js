c4316129f11113773c3ab4b953a4dc1e7eabdc10
Graham Knop <haarg@haarg.org>
2014-09-13 14:37:02 -0400

    better sticky sidebar, closer to before my rewrite

--- root/static/js/toolbar.js
+++ root/static/js/toolbar.js
@@ -9,19 +9,20 @@ if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
 
         $(window).scroll(function(e) {
             var scrollTop = $(this).scrollTop();
+            var screenHeight = $(this).height();
             var contentTop = content.offset().top;
             var contentHeight = content.height();
-            if (height > contentHeight) {
+            if (height > contentHeight || scrollTop < contentTop + (height > screenHeight ? height - screenHeight : 0)) {
                 el.addClass("sticky-panel-top").removeClass("sticky-panel-bottom sticky-panel-sticky");
             }
             else if (scrollTop + height > contentTop + contentHeight) {
                 el.addClass("sticky-panel-bottom").removeClass("sticky-panel-top sticky-panel-sticky");
             }
-            else if (scrollTop > contentTop) {
-                el.addClass("sticky-panel-sticky").removeClass("sticky-panel-top sticky-panel-bottom");
+            else if (height > screenHeight) {
+                el.addClass("sticky-panel-bottom sticky-panel-sticky").removeClass("sticky-panel-top");
             }
             else {
-                el.addClass("sticky-panel-top").removeClass("sticky-panel-bottom sticky-panel-sticky");
+                el.addClass("sticky-panel-sticky sticky-panel-top").removeClass("sticky-panel-bottom");
             }
         });
     });
