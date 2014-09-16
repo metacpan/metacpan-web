faac8b4be89c6cfdf0a3ad535ae7f7cee4198c26
Graham Knop <haarg@haarg.org>
2014-09-16 00:57:16 -0400

    hide/show pod by class toggle

--- root/static/js/cpan.js
+++ root/static/js/cpan.js
@@ -28,26 +28,6 @@ $.extend({
     }
 });
 
-var podVisible = false;
-
-function togglePod(lines) {
-    var toggle = podVisible ? 'none' : 'block';
-    podVisible = !podVisible;
-    if (!lines || !lines.length) return;
-    for (var i = 0; i < lines.length; i++) {
-        var start = lines[i][0],
-        length = lines[i][1];
-        var sourceC = $('.container')[0].children;
-        var linesC = $('.gutter')[0].children;
-        var x;
-        for (x = start; x < start + length; x++) {
-            sourceC[x].style.display = toggle;
-            linesC[x].style.display = toggle;
-        }
-
-    }
-}
-
 function togglePanel(side) {
     var panel = $('#' + side + '-panel');
     var shower = $('#show-' + side + '-panel');
