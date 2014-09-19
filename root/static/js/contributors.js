b6ed0775c98458888cda022626eb29af0ff632b5
Graham Knop <haarg@haarg.org>
2014-09-19 11:04:07 -0400

    improve handling of contributors gravatars
    Place the gravatars inside the links, so when active the gravatars still
    show.  Also make the javascript idempotent, and handle the gravatar not
    existing.

--- root/static/js/contributors.js
+++ root/static/js/contributors.js
@@ -10,22 +10,31 @@ $(function(){
             if ( typeof data.name == 'undefined' ) {
                 return;
             }
-            // TODO make this an :before pseudo-class
-            var gravatar = data.gravatar_url;
-            gravatar = gravatar.replace( 
-                "^https?://([a-z0-9.-]+\.)?gravatar\.com/",
-                "https://secure.gravatar.com/",
-                "i"
-            ).replace(
-                /s=\d+/,
-                's=20'
-            );
-            var $img = $('<img />').attr( 'src', gravatar )
-                .attr( 'width', 20 )
-                .attr( 'height', 20 );
+            $anchor.attr( 'data-cpan-author', data.pauseid );
+            $anchor.attr( 'href', '/author/' + data.pauseid );
             $anchor.text( data.name );
-            $anchor.parent().addClass('gravatar');
-            $anchor.before( $img );
+
+            var gravatar = data.gravatar_url;
+            if (gravatar) {
+                gravatar = gravatar.replace(
+                    "^https?://([a-z0-9.-]+\.)?gravatar\.com/",
+                    "https://secure.gravatar.com/",
+                    "i"
+                ).replace(
+                    /s=\d+/,
+                    's=20'
+                );
+
+                var $img = $anchor.find('img.gravatar');
+                if (!$img.length) {
+                    $img = $('<img />')
+                        .attr( 'width', 20 )
+                        .attr( 'height', 20 );
+                    $anchor.prepend( $img );
+                }
+                $img.addClass( 'gravatar' );
+                $img.attr( 'src', gravatar )
+            }
         });
     });
 });
