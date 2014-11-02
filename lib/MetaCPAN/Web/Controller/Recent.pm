890127a4a272d9b11d5c3a9ab1417ac103889207
Gabor Szabo <gabor@szabgab.com>
2014-11-02 09:34:37 +0200

    implement paging for the /recent page #1356

--- lib/MetaCPAN/Web/Controller/Recent.pm
+++ lib/MetaCPAN/Web/Controller/Recent.pm
@@ -3,19 +3,30 @@ use strict;
 use warnings;
 use base 'MetaCPAN::Web::Controller';
 
+use MetaCPAN::Web::Types qw( PositiveInt );
+
 sub index : Path {
     my ( $self, $c ) = @_;
+    my $req = $c->req;
+
+    my $page_size = $req->param('size');
+    unless ( is_PositiveInt($page_size) && $page_size <= 500 ) {
+        $page_size = 100;
+    }
+
     my ($data)
         = $c->model('API::Release')
-        ->recent( $c->req->page, $c->req->params->{f} || 'l' )->recv;
+        ->recent( $c->req->page, $page_size, $c->req->params->{f} || 'l' )
+        ->recv;
     my $latest = [ map { $_->{fields} } @{ $data->{hits}->{hits} } ];
     $c->res->last_modified( $latest->[0]->{date} ) if (@$latest);
     $c->stash(
         {
-            recent   => $latest,
-            took     => $data->{took},
-            total    => $data->{hits}->{total},
-            template => 'recent.html'
+            recent    => $latest,
+            took      => $data->{took},
+            total     => $data->{hits}->{total},
+            template  => 'recent.html',
+            page_size => $page_size,
         }
     );
 }
