cb4fb9641842d6976b146d2bc7caaed1dc4efd9d
Mateu X Hunter <hunter@missoula.org>
2015-01-09 09:51:49 -0700

    More explicit naming

--- lib/MetaCPAN/Web/Controller.pm
+++ lib/MetaCPAN/Web/Controller.pm
@@ -6,6 +6,6 @@ BEGIN { extends 'Catalyst::Controller'; }
 
 # For elasticsearch 1.x changes in the structure returned in the fields of {hits}{hits}
 # See: http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/_return_values.html
-with('MetaCPAN::Web::Role::Adapter');
+with('MetaCPAN::Web::Role::Elasticsearch::Adapter');
 
 __PACKAGE__->meta->make_immutable;
