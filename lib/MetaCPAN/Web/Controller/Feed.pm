bfd9e637fffa8068e6c5672428f50a2103e0a4b3
Micha Nasriachi <micha.nasriachi@booking.com>
2016-04-07 15:46:42 +0200

    less code duplications

--- lib/MetaCPAN/Web/Controller/Feed.pm
+++ lib/MetaCPAN/Web/Controller/Feed.pm
@@ -9,6 +9,7 @@ use HTML::Escape qw/escape_html/;
 use DateTime::Format::ISO8601;
 use Path::Tiny qw/path/;
 use Text::Markdown qw/markdown/;
+use MetaCPAN::Web::Util qw( fix_structure );
 
 sub recent : Local : Args(0) {
     my ( $self, $c ) = @_;
@@ -109,11 +110,7 @@ sub distribution : Local : Args(1) {
 
 sub build_entry {
     my ( $self, $entry ) = @_;
-    ref $entry eq 'HASH' or warn 'WRONG ENTRY TYPE';
-    for my $k ( keys %{ $entry } ) {
-        next unless ref $entry->{$k} eq 'ARRAY';
-        $entry->{$k} = $entry->{$k}[0];
-    }
+    $entry = fix_structure($entry);
     my $e = XML::Feed::Entry->new('RSS');
     $e->title( $entry->{name} );
     $e->link(
