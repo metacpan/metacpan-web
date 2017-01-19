acabc8af1769f8f35cee778615be03212425028f
Leo Lapworth <leo@cuckoo.org>
2017-01-19 18:55:44 +0000

    Fix /sysadmin/datacenters/list - which got broke in our purge refactor
    When MooseX::Fastly::Role stripped out the cdn_api() and we stopped
    using it elsewhere this end point got broke.
    
    I've uploaded a new MooseX::Fastly::Role which adds cdn_api() back,
    even if the purging itself is done through HTTP::Tiny, having the
    api accessible is still usefil (as here).

--- lib/MetaCPAN/Web/Controller/SysAdmin/DataCenters.pm
+++ lib/MetaCPAN/Web/Controller/SysAdmin/DataCenters.pm
@@ -7,12 +7,21 @@ BEGIN { extends 'MetaCPAN::Web::Controller' }
 # This is used by http://mirrors.cpan.org/ fetch our
 # datacenters and update the official CPAN list
 
+# /sysadmin/datacenters/list
 sub list_datacenters : Path('list') : Args(0) GET {
     my ( $self, $c ) = @_;
 
     # Fetch the data center info from fastly
     # XXX: will not work unless you have the credentials
-    my $datacenters = $c->datacenters;
+    my $datacenters;
+
+    if ( my $net_fastly = $c->cdn_api() ) {
+
+        # Uses the private interface as fastly client doesn't
+        # have this end point yet
+        $datacenters = $net_fastly->client->_get('/datacenters');
+
+    }
 
     $c->add_surrogate_key('datacenters');
     $c->cdn_max_age('1d');
