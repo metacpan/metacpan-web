1ec622d95af759cd0bd9025ff6566238a15e2a30
Graham Knop <haarg@haarg.org>
2017-11-18 10:25:44 -0600

    remove unused method

--- lib/MetaCPAN/Web/Model/API.pm
+++ lib/MetaCPAN/Web/Model/API.pm
@@ -60,12 +60,6 @@ sub COMPONENT {
     return $self->SUPER::COMPONENT( $app, $config );
 }
 
-sub model {
-    my ( $self, $model ) = @_;
-    return MetaCPAN::Web->model('API') unless $model;
-    return MetaCPAN::Web->model("API::$model");
-}
-
 sub request {
     my ( $self, $path, $search, $params, $method ) = @_;
 
