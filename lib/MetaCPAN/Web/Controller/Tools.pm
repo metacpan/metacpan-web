d2919c384645dd17dbd3e9603146df0f3f2e7d04
Graham Knop <haarg@haarg.org>
2017-12-11 09:40:11 -0600

    lab -> tools

---
+++ lib/MetaCPAN/Web/Controller/Tools.pm
@@ -0,0 +1,14 @@
+package MetaCPAN::Web::Controller::Tools;
+
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'MetaCPAN::Web::Controller' }
+
+sub tools : Path : Args(0) {
+    my ( $self, $c ) = @_;
+    $c->stash( template => 'tools.html' );
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
