b2c27f6b56c22b12f28565c92c4b19b1b7357e38
Zak B. Elep <zakame@zakame.net>
2018-04-22 01:52:15 +0800

    Add Model::API::ContributingDoc for requesting contributing docs
    This adds a new model class that requests from the backend API for the
    CONTRIBUTING/HACKING docs of a release, if any.
    
    c.f. https://github.com/metacpan/metacpan-api/commit/088bd9eba0abc8d95b946bdd08a93341a6c736a9
    
    Add `/contribute-to/$author/$release` route
    
    This route uses API::ContributingDoc to request for the Contributing
    file, if available, and then redirects to the appropriate view (render
    HTML if its pod, or leave as source if otherwise.)
    
    Add `How to Contribute` link in release-info sidebar
    
    This generates the appropriate `/contributing-to/` link for a
    module/release, to enable
    https://github.com/metacpan/metacpan-web/issues/1779.
    
    :lipstick: `make_immutable`
    
    Use `interesting_files` instead of a new backend API
    
    Use https://github.com/metacpan/metacpan-api/pull/803 instead of the new
    `contributing_doc` call in
    https://github.com/metacpan/metacpan-api/pull/802 and just filter for
    CONTRIBUTING/HACKING files on the controller.
    
    :lipstick: Refactor for clarity
    
    Use API::Release->interesting_files instead of API::ContributingDoc
    
    The latter module seems not needed here anyway as the underlying
    `/contributing_doc` endpoint is gone.  Just let the ContributingDoc
    controller do its thing instead.
    
    :lipstick: Remove obsolete API::ContributingDoc module
    
    :lipstick: Interpolate instead of concatenate
    
    Render *.pm variants of Contributing/Hacking as pod
    
    Take into account
    https://github.com/metacpan/metacpan-api/pull/803/commits/9749e0033e825bb91047e0866d04ee422b7ff1bc

---
+++ lib/MetaCPAN/Web/Controller/ContributingDoc.pm
@@ -0,0 +1,60 @@
+package MetaCPAN::Web::Controller::ContributingDoc;
+
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'MetaCPAN::Web::Controller' }
+
+use List::Util ();
+
+sub index : Chained('/') : PathPart('contributing-to') : CaptureArgs(0) {
+}
+
+sub release : Chained('index') : PathPart('') : Args(2) {
+    my ( $self, $c, $author, $release ) = @_;
+
+    # force consistent casing in URLs
+    if ( $author ne uc($author) ) {
+        $c->res->redirect(
+            $c->uri_for( $c->action, [ uc($author), $release ] ), 301 );
+        $c->detach();
+    }
+
+    $c->forward( 'get', [ $author, $release ] );
+}
+
+sub get : Private {
+    my ( $self, $c, @args ) = @_;
+
+    my $contributing_re = qr/(CONTRIBUTING|HACKING)/i;
+    my $files
+        = $c->model('API::Release')->interesting_files(@args)->get->{files};
+
+    my $file = List::Util::first { $_->{name} =~ /$contributing_re/ } @$files;
+
+    if ( !exists $file->{path} ) {
+        my $release = join q{/}, @args;
+        $c->stash( {
+            message => 'Ask the author on how to contribute to this release.',
+            suggest => {
+                description => 'Try the release info page',
+                url         => $c->uri_for("/release/$release"),
+                link_text   => $release,
+            }
+        } );
+        $c->detach('/not_found');
+    }
+    else {
+        my $path = join '/' => @$file{qw(author release path)};
+        if ( $path =~ /\.(pod|pm)$/ ) {
+            $c->res->redirect( "/pod/release/$path", 301 );
+        }
+        else {
+            $c->res->redirect( "/source/$path", 301 );
+        }
+    }
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
