6b284619ff0b12da1dbcac247a4b5984ec6c3884
Patrick Cronin <pcronin@maxmind.com>
2019-10-17 22:08:59 -0400

    Allow reuse from another controller

--- lib/MetaCPAN/Web/Controller/Pod.pm
+++ lib/MetaCPAN/Web/Controller/Pod.pm
@@ -4,9 +4,8 @@ use Moose;
 
 use Encode qw( encode decode DIE_ON_ERR LEAVE_SRC );
 use Future;
-use HTML::Escape qw(escape_html);
-use HTML::Restrict   ();
 use HTML::TokeParser ();
+use MetaCPAN::Web::RenderUtil 'filter_html';
 use Try::Tiny qw( try );
 use URI ();
 
@@ -136,7 +135,7 @@ sub view : Private {
         }
     )->get;
 
-    my $pod_html = $self->filter_html( $pod->{raw}, $data );
+    my $pod_html = filter_html( $pod->{raw}, $data );
 
     my $release = $c->stash->{release};
 
@@ -197,7 +196,7 @@ sub pod2html : Path('/pod2html') {
         'POST'
     )->get->{raw};
 
-    $html = $self->filter_html($html);
+    $html = filter_html($html);
 
     if ( $c->req->parameters->{raw} ) {
         $c->res->content_type('text/html');
@@ -228,125 +227,6 @@ sub pod2html : Path('/pod2html') {
             ( $abstract ? ( abstract => $abstract ) : () ),
         } );
     }
-}
-
-sub filter_html {
-    my ( $self, $html, $data ) = @_;
-
-    my $hr = HTML::Restrict->new(
-        uri_schemes =>
-            [ undef, 'http', 'https', 'data', 'mailto', 'irc', 'ircs' ],
-        rules => {
-            a       => [qw( href id target )],
-            b       => [],
-            br      => [],
-            caption => [],
-            center  => [],
-            code    => [ { class => qr/^language-\S+$/ } ],
-            dd      => [],
-            div     => [ { class => qr/^pod-errors(?:-detail)?$/ } ],
-            dl      => [],
-            dt      => ['id'],
-            em      => [],
-            h1      => ['id'],
-            h2      => ['id'],
-            h3      => ['id'],
-            h4      => ['id'],
-            h5      => ['id'],
-            h6      => ['id'],
-            i       => [],
-            li      => ['id'],
-            ol      => [],
-            p       => [],
-            pre     => [ {
-                class        => qr/^line-numbers$/,
-                'data-line'  => qr/^\d+(?:-\d+)?(?:,\d+(?:-\d+)?)*$/,
-                'data-start' => qr/^\d+$/,
-            } ],
-            span   => [ { style => qr/^white-space: nowrap;$/ } ],
-            strong => [],
-            sub    => [],
-            sup    => [],
-            table  => [ qw( border cellspacing cellpadding align ), ],
-            tbody  => [],
-            th     => [],
-            td     => [],
-            tr     => [],
-            u      => [],
-            ul     => [ { id => qr/^index$/ } ],
-
-            #
-            # SVG tags.
-            #
-            circle   => [qw(id cx cy r style transform)],
-            clippath => [qw(id clippathunits style transform)],
-            defs     => [qw(id style transform)],
-            ellipse  => [qw(id cx cy rx ry style transform)],
-            g        => [qw(id style transform)],
-            line     => [qw(id style transform x1 y1 x2 y2)],
-            marker   => [
-                qw(id markerheight markerunits markerwidth orient refx refy)],
-            mask =>
-                [qw(id height maskunits maskcontentunits style x y width)],
-            lineargradient => [
-                qw(id gradientunits gradienttransform spreadmethod
-                    x1 x2 y1 y2 xlink:href)
-            ],
-            path           => [qw(id d pathlength style transform)],
-            polygon        => [qw(id points style transform)],
-            polyline       => [qw(id points style transform)],
-            radialgradient => [
-                qw(id gradientunits gradienttransform spreadmethod
-                    cx cy fx fy r xlink:href)
-            ],
-            rect => [qw(id height style transform x y width)],
-            stop => [qw(id offset style)],
-            svg  => [ qw(id height preserveaspectratio version viewbox
-                    width xmlns xmlns:xlink) ],
-            title => [qw(id style)],
-            use   => [qw(id height transform width x xlink xlink:href y)],
-        },
-        replace_img => sub {
-
-            # last arg is $text, which we don't need
-            my ( $tagname, $attrs, undef ) = @_;
-            my $tag = '<img';
-            for my $attr (qw( alt border height width src title)) {
-                next
-                    unless exists $attrs->{$attr};
-                my $val = $attrs->{$attr};
-                if ( $attr eq 'src' ) {
-                    if ( $val =~ m{^(?:(?:https?|ftp):)?//|^data:} ) {
-
-                        # use directly
-                    }
-                    elsif ( $val =~ /^[0-9a-zA-Z.+-]+:/ ) {
-
-                        # bad protocol
-                        return '';
-                    }
-                    elsif ($data) {
-                        my $base = "https://st.aticpan.org/source/";
-                        if ( $val =~ s{^/}{} ) {
-                            $base .= "$data->{author}/$data->{release}/";
-                        }
-                        else {
-                            $base .= $data->{associated_pod}
-                                || "$data->{author}/$data->{release}/$data->{path}";
-                        }
-                        $val = URI->new_abs( $val, $base )->as_string;
-                    }
-                    else {
-                        $val = '/static/images/gray.png';
-                    }
-                }
-                $tag .= qq{ $attr="} . escape_html($val) . qq{"};
-            }
-            $tag .= ' />';
-            return $tag;
-        },
-    );
-    $hr->process($html);
 }
 
 __PACKAGE__->meta->make_immutable;
