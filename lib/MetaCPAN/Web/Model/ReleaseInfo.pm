7866904ca299701bfa850c10a9e0456f73109bc4
lnation <thisusedtobeanemail@gmail.com>
2019-11-08 15:44:55 +0800

    highlight modules or dists with ADOPTME or HANDOFF or NEEDHELP

--- lib/MetaCPAN/Web/Model/ReleaseInfo.pm
+++ lib/MetaCPAN/Web/Model/ReleaseInfo.pm
@@ -3,7 +3,6 @@ package MetaCPAN::Web::Model::ReleaseInfo;
 use strict;
 use warnings;
 use MetaCPAN::Moose;
-
 extends 'Catalyst::Model';
 
 use List::Util qw( all max );
@@ -20,6 +19,7 @@ my %models = (
     _changes      => 'API::Changes',
     _rating       => 'API::Rating',
     _favorite     => 'API::Favorite',
+    _permission   => 'API::Permission'
 );
 
 has [ keys %models ] => ( is => 'ro' );
@@ -46,6 +46,9 @@ sub find {
         $self->_wrap(
             release => $data,
             %dist_data,
+            permission => $self->_permission->get(
+                'module', $data->{release}{main_module}
+            ),
             $self->_release_data(
                 $data->{release}{author},
                 $data->{release}{name}
@@ -55,7 +58,7 @@ sub find {
 }
 
 sub get {
-    my ( $self, $author, $release_name ) = @_;
+    my ( $self, $author, $release_name, $module_name ) = @_;
     my $release      = $self->_release->get( $author, $release_name );
     my %release_data = $self->_release_data( $author, $release_name );
     $release->then( sub {
@@ -67,6 +70,9 @@ sub get {
         $self->_wrap(
             release => $data,
             %release_data,
+            permission => $self->_permission->get(
+                'module', $module_name || $data->{release}{main_module}
+            ),
             $self->_dist_data( $data->{release}{distribution} ),
         );
     } )->then( $self->normalize );
@@ -131,6 +137,8 @@ sub normalize {
                 grep is_hashref($_),
                 values %$data
             ),
+            notification =>
+                $self->normalise_notification_type( $data->{permission} ),
             coverage     => $data->{coverage},
             release      => $data->{release}{release},
             favorites    => $data->{favorites}{favorites}{$dist},
@@ -192,7 +200,7 @@ sub groom_irc {
                 = 'irc://'
                 . $host
                 . (
-                  $ssl ? q{:+} . ( $port || 6697 )
+                  $ssl  ? q{:+} . ( $port || 6697 )
                 : $port ? ":$port"
                 :         q{}
                 )
@@ -270,6 +278,16 @@ sub normalize_issue_url {
     }{https://rt.cpan.org/Dist/Display.html?Name=}x;
 
     return $url;
+}
+
+sub normalise_notification_type {
+    my ( $self, $permission ) = @_;
+    return unless $permission && $permission->{co_maintainers};
+    for ( reverse @{ $permission->{co_maintainers} } ) {
+        if ( $_ =~ m/NEEDHELP|ADOPTME|HANDOFF/ ) {
+            return $_;
+        }
+    }
 }
 
 __PACKAGE__->meta->make_immutable;
