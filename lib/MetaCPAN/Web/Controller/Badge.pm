f9c4306b34f05744c6fc3439202c1c36499f9586
Thibault DUPONCHELLE <thibault.duponchelle@gmail.com>
2021-07-23 12:06:48 +0200

    Add hosted badge to MetaCPAN

---
+++ lib/MetaCPAN/Web/Controller/Badge.pm
@@ -0,0 +1,28 @@
+package MetaCPAN::Web::Controller::Badge;
+
+use Moose;
+
+BEGIN { extends 'MetaCPAN::Web::Controller' }
+
+use DateTime;
+
+sub dist : Chained('/dist/root') PathPart('badge.svg') Args(0) {
+    my ( $self, $c ) = @_;
+    my $dist = $c->stash->{distribution_name};
+
+    $c->forward( 'badge', [ distribution => $dist ] );
+}
+
+sub badge : Private {
+    my ( $self, $c, %args ) = @_;
+
+    $c->res->content_type('image/svg+xml');
+    $c->res->headers->expires( time + 86400 );
+    $c->stash( {
+        template => 'badge.svg.tx',
+    } );
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
