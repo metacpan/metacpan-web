9319ac379709aac85750c9b2b14b2cc92a796b39
Graham Knop <haarg@haarg.org>
2024-05-14 01:18:11 +0200

    fix various onclick handlers by explicitly attaching functions to window

--- root/static/js/inject.js
+++ root/static/js/inject.js
@@ -2,6 +2,8 @@ import jQuery from 'jquery';
 import { SyntaxHighlighter } from './shCore.js';
 
 window.SyntaxHighlighter = SyntaxHighlighter;
+window.$ = jQuery;
+window.jQuery = jQuery;
 
 export {
     jQuery as default,
